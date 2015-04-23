448c2080a4cfa965e2b69e9f7b32d1bd0a2f1ea8
digiholic <digikun@gmail.com>
2015-04-23 16:16:44 -0600

    Got to work on the music objects. They store the location of the song, the display name, and probability. Multiple .tstm files can point to the same song, and they'll be put in a folder for a stage to determine the MyMusic for that stage.
    Also included is the NCS release of the song that's likely to become TUSSLE's main menu theme.

--- engine/musicManager.py
+++ engine/musicManager.py
@@ -4,26 +4,57 @@ import cPickle as pickle
 import settingsManager
 
 
-class musicManager:
+class MusicManager:
     def __init__(self):
         self.myMusic = []
+        self.directory = os.path.join(os.path.dirname(settingsManager.__file__),"music")
+    
+    def createSongData(self,path,name,prob):
+        song = SongData(path,name,prob)
+        self.saveSongData(song)
         
-    def createSongData(self,music):
-        f = open(music.name+".tstm",'w')
+    def saveSongData(self,music):
+        f = open(os.path.join(self.directory,music.name+".tstm"),'w+')
         pickle.dump(music, f)
         
     def loadSongData(self,fname):
-        f = open(fname + ".tstm",'r')
+        f = open(os.path.join(self.directory,fname+".tstm"),'r+')
         ret = pickle.load(f)
         return ret
     
-class songData:
-    def __init__(self,path,name):
-        self.music = pygame.mixer.music.load(path)
+class SongData:
+    def __init__(self,path,name, prob):
+        self.music = path
         self.name = name
+        self.probability = prob
+        pygame.mixer.init()
         
     def play(self):
-        self.music.play(-1)
+        pygame.mixer.music.load(self.music)
+        pygame.mixer.music.play()
         
     def stop(self):
-        self.music.stop()
+        pygame.mixer.music.stop()
+
+def test():
+    musicManager = MusicManager()
+    musicManager.createSongData("/home/digiholic/workspace/smashSystem/music/The Void - Lost Language (Original Edit).mp3", "The Void - Lost Language (NCS Release)", 100)
+    song = musicManager.loadSongData("The Void - Lost Language (NCS Release)")
+    pygame.init()
+    pygame.mixer.init()
+    
+    song.play()
+        
+    screen = pygame.display.set_mode([640,480])
+    pygame.display.set_caption("USS Sprite Viewer")
+    
+    while 1:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return -1
+            if event.type == pygame.KEYDOWN:
+                song.stop()
+            
+        screen.fill([100, 100, 100])
+        pygame.display.flip()
+if __name__  == '__main__': test()      
