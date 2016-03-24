62b2d12871ce1bb95717a41375977c86c3417350
Eugene Wang <eaglgenes101@gmail.com>
2016-03-24 16:54:03 -0400

    Woops, broke something

--- engine/baseActions.py
+++ engine/baseActions.py
@@ -168,7 +168,7 @@ class Crouch(action.Action):
         elif actor.keysContain(invkey):
             actor.setPreferredSpeed(-actor.var['crawlSpeed'], actor.getFacingDirection())
         else:
-            actor.setPreferredSpeed(0)
+            actor.setPreferredSpeed(0, actor.getFacingDirection())
         
         self.frame += 1
         if self.frame > self.lastFrame: self.frame = 0
@@ -178,7 +178,7 @@ class CrouchGetup(action.Action):
         action.Action.__init__(self, length)
 
     def update(self, actor):
-        actor.setPreferredSpeed(0)
+        actor.setPreferredSpeed(0, actor.getFacingDirection)
         if actor.grounded == False:
             actor.doFall()
         elif actor.bufferContains('down') and self.frame > 0:
