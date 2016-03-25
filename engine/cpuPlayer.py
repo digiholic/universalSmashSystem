8f3e630d01369bb5cab0b573636086f20e44a38d
Eugene Wang <eaglgenes101@gmail.com>
2016-03-24 23:44:49 -0400

    Made AI following somewhat more sensible

--- engine/cpuPlayer.py
+++ engine/cpuPlayer.py
@@ -1,7 +1,10 @@
+import engine.baseActions as baseActions
+
 class CPUplayer():
     def __init__(self):
         self.mode = 'duckling'
         self.keysHeld = []
+        self.jump_last_frame = 0
     
     def loadGameState(self,fighter,stage,players):
         self.fighter = fighter
@@ -32,16 +35,24 @@ class CPUplayer():
         """
         if self.mode == 'duckling':
             dx, dy = self.getDistanceTo(self.players[0])
-            if dx < 0:
+            if dx < 0 and abs(dx)>abs(dy)//2:
                 self.pressButton('left')
             else:
                 self.releaseButton('left')
                 
-            if dx > 0:
+            if dx > 0 and abs(dx)>abs(dy)//2:
                 self.pressButton('right')
             else:
                 self.releaseButton('right')
-            if dy < 0:
+
+            if dy < 0 and abs(dy)>abs(dx)//2 and self.jump_last_frame > 8 and self.fighter.change_y>=-1:
                 self.pressButton('jump')
+                self.jump_last_frame = 0
             else:
                 self.releaseButton('jump')
+                self.jump_last_frame += 1
+
+            if dy > 0 and abs(dy)>abs(dx)//2 and self.fighter.grounded and not isinstance(self.fighter.current_action, baseActions.Crouch):
+                self.pressButton('down')
+            else:
+                self.releaseButton('down')
