d45a31012677296e5afb25add68322135dd87628
Eugene Wang <eaglgenes101@gmail.com>
2016-04-20 20:08:17 -0400

    Working on making cx_freeze work

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 from cx_Freeze import setup, Executable
+import sys
 
 # Dependencies are automatically detected, but it might need
 # fine tuning.
-buildOptions = dict(packages = ['numpy', 'pygame', 'requests'], excludes = [])
+buildOptions = dict(packages = ['numpy', 'pygame', 'requests'], excludes = [], include_files = ['rexlia rg.ttf', 'full Pack 2025.ttf', 'settings', 'sprites/', 'music/', 'fighters/', 'stages/'])
+
+base = None
+if sys.platform == "win32":
+    base = "Win32GUI"
 
 executables = [
-    Executable('main.py', 'Console', targetName = 'main.exe')
+    Executable('main.py', base, targetName = 'main.exe')
 ]
 
 setup(name='TUSSLE',
