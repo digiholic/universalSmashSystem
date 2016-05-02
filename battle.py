07e4ca641039cc8884e286f9997476a1a45978db
Eugene Wang <eaglgenes101@gmail.com>
2016-05-01 21:57:51 -0400

    Work done on clanking

--- battle.py
+++ battle.py
@@ -200,12 +200,18 @@ class Battle():
                 for other in hitbox_clank:
                     print('Other hitbox: '+str(other))
                     if not hbox.compareTo(other):
-                        if hasattr(hbox.owner,'current_action') and hbox.article == None and isinstance(other, hitbox.DamageHitbox):
+                        if hasattr(hbox.owner,'current_action') and hbox.article == None and (isinstance(other, hitbox.DamageHitbox) or isinstance(other, hitbox.GrabHitbox)):
+                            hbox.owner.hitstop = 12
+                            other.owner.hitstop = 12
+                            other.kill()
                             hbox.owner.current_action.onClank(hbox.owner)
                         print("CLANK!")
                         other.owner.lockHitbox(hbox)
                     if not other.compareTo(hbox):
-                        if hasattr(other.owner,'current_action') and other.article == None and isinstance(hbox, hitbox.DamageHitbox):
+                        if hasattr(other.owner,'current_action') and other.article == None and (isinstance(hbox, hitbox.DamageHitbox) or isinstance(other, hitbox.GrabHitbox)):
+                            hbox.owner.hitstop = 12
+                            other.owner.hitstop = 12
+                            hbox.kill()
                             other.owner.current_action.onClank(other.owner)
                         print("CLANK!")
                         hbox.owner.lockHitbox(other)
