20c30d369b7546cd198f0ba128f5418c438ed8d5
digiholic <digikun@gmail.com>
2016-07-29 13:44:24 -0600

    Fixed Sandbag crash and gave him a real icon

--- fighters/sandbag/fighter.py
+++ fighters/sandbag/fighter.py
@@ -2,6 +2,7 @@ import engine.abstractFighter as abstractFighter
 import spriteManager
 import os
 import settingsManager
+import engine.baseActions as baseActions
 
 class Fighter(abstractFighter.AbstractFighter):
     def __init__(self,baseDir,playerNum):
@@ -25,151 +26,10 @@ class Fighter(abstractFighter.AbstractFighter):
         
         abstractFighter.AbstractFighter.__init__(self,baseDir,playerNum)
         
-        self.actions = settingsManager.importFromURI(__file__,'sandbag_actions.py')
+        self.actions = baseActions
         
         self.current_action = self.actions.NeutralAction()
     
-    
-    def doIdle(self):
-        self.changeAction(self.actions.NeutralAction())
-
-    def doCrouch(self):
-        pass
-
-    def doCrouchGetup(self):
-        pass
-    
-    def doFall(self):
-        self.changeAction(self.actions.Fall())
-
-    def doHelpless(self):
-        pass
-    
-    def doPlatformDrop(self):
-        pass
-          
-    def doLand(self):
-        pass
-
-    def doHelplessLand(self):
-        pass
-        
-    def doStop(self):
-        pass
-
-    def doRunStop(self):
-        pass
-            
-    def doGroundMove(self,direction):
-        pass
-
-    def doDash(self,direction):
-        pass
-        
-    #def doRun(self,direction):
-    #    pass
-        
-    def doPivot(self):
-        pass
-
-    def doRunPivot(self):
-        pass
-    
-    def doJump(self):
-        pass
-
-    def doAirJump(self):
-        pass
-
-    def doPreShield(self):
-        pass
-                
-    def doShield(self):
-        pass
-
-    def doShieldStun(self, length):
-        pass
-        
-    def doForwardRoll(self):
-        pass
-    
-    def doBackwardRoll(self):
-        pass
-        
-    def doSpotDodge(self):
-        pass
-        
-    def doAirDodge(self):
-        pass
-
-    def doTechDodge(self):
-        pass
-     
-    def doLedgeGrab(self,ledge):
-        pass
-
-    def doLedgeGetup(self):
-        pass
-
-    def doLedgeAttack(self):
-        pass
-
-    def doLedgeRoll(self):
-        pass
-
-    def doGroundGrab(self):
-        pass
-
-    def doGrabbing(self):
-        pass
-
-    def doTrapped(self, length):
-        pass
-
-    def doStunned(self, length):
-        pass
-
-    def doGrabbed(self, height):
-        pass
-
-    def doPummel(self):
-        pass
-
-    def doThrow(self):
-        pass
-        
-    def doGroundAttack(self):
-        pass
-
-    def doDashAttack(self):
-        pass
-
-    def doDashGrab(self):
-        pass
-    
-    def doAirAttack(self):
-        pass
-
-    def doGetupAttack(self, direction):
-        pass
-    
-    def doGroundSpecial(self):
-        pass
-
-    def doAirSpecial(self):
-        pass
-
-    def doHitStun(self,hitstun,trajectory,hitstop):
-        pass
-
-    def doTryTech(self, hitstun, trajectory, hitstop):
-        pass
-
-    def doTrip(self, length, direction):
-        pass
-
-    def doGetup(self, direction):
-        pass    
 def cssIcon(): return None
 
 def getFighter(baseDir,playerNum):
