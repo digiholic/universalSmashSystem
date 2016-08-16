4bc52e559926eaec611dab746c45aaacfbaaccaf
Eugene Wang <eaglgenes101@gmail.com>
2016-08-16 14:18:05 -0700

    Spotted oversight in some input methods

--- engine/abstractFighter.py
+++ engine/abstractFighter.py
@@ -1052,7 +1052,7 @@ class AbstractFighter():
     #A key tap (press, then release)
     def keyTapped(self, _key, _from = 8, _state = 0.1, _to = 0):
         down_frames = map(lambda k: _key in k and k[_key] >= _state, self.input_buffer.getLastNFrames(_from, _to))
-        up_frames = map(lambda k: _key in k and k[_key] < _state, self.input_buffer.getLastNFrames(_from, _to))
+        up_frames = map(lambda k: _key not in k or k[_key] < _state, self.input_buffer.getLastNFrames(_from, _to))
         if not any(down_frames) or not any(up_frames):
             return False
         first_down_frame = reduce(lambda j, k: j if j != None else (k if down_frames[k] else None), range(len(down_frames)), None)
@@ -1062,7 +1062,7 @@ class AbstractFighter():
     #A key press which hasn't been released yet
     def keyHeld(self, _key, _from = 8, _state = 0.1, _to = 0):
         down_frames = map(lambda k: _key in k and k[_key] >= _state, self.input_buffer.getLastNFrames(_from, _to))
-        up_frames = map(lambda k: _key in k and k[_key] < _state, self.input_buffer.getLastNFrames(_from, _to))
+        up_frames = map(lambda k: _key not in k or k[_key] < _state, self.input_buffer.getLastNFrames(_from, _to))
         if not any(down_frames):
             return False
         if any(down_frames) and not any(up_frames):
@@ -1073,11 +1073,11 @@ class AbstractFighter():
 
     #A key release
     def keyUp(self, _key, _from = 1, _state = 0.1, _to = 0):
-        return any(map(lambda k: _key in k and k[_key] < _state, self.input_buffer.getLastNFrames(_from, _to)))
+        return any(map(lambda k: _key not in k or k[_key] < _state, self.input_buffer.getLastNFrames(_from, _to)))
 
     #A key reinput (release, then press)
     def keyReinput(self, _key, _from = 8, _state = 0.1, _to = 0):
-        up_frames = map(lambda k: _key in k and k[_key] < _state, self.input_buffer.getLastNFrames(_from, _to))
+        up_frames = map(lambda k: _key not in k or k[_key] < _state, self.input_buffer.getLastNFrames(_from, _to))
         down_frames = map(lambda k: _key in k and k[_key] >= _state, self.input_buffer.getLastNFrames(_from, _to))
         if not any(down_frames) or not any(down_frames):
             return False
@@ -1087,7 +1087,7 @@ class AbstractFighter():
 
     #A key release which hasn't been pressed yet
     def keyIdle(self, _key, _from = 8, _state = 0.1, _to = 0):
-        up_frames = map(lambda k: _key in k and k[_key] < _state, self.input_buffer.getLastNFrames(_from, _to))
+        up_frames = map(lambda k: _key not k or k[_key] < _state, self.input_buffer.getLastNFrames(_from, _to))
         down_frames = map(lambda k: _key in k and k[_key] >= _state, self.input_buffer.getLastNFrames(_from, _to))
         if not any(up_frames):
             return False
