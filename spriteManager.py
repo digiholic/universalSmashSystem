6eafff506bdefcb519da902bf82e5c7164a65263
digiholic <digikun@gmail.com>
2016-08-25 20:56:49 -0600

    Scaled sprites center when scaled now.

--- spriteManager.py
+++ spriteManager.py
@@ -192,10 +192,13 @@ class ImageSprite(Sprite):
         self.changed = True
         
     def scale(self,_scaleFactor):
+        old_rect = self.rect.copy()
         w = int(self.image.get_width() * _scaleFactor)
         h = int(self.image.get_height() * _scaleFactor)
         pygame.transform.scale(self.image, (w,h))
+        self.rect = self.image.get_rect()
         
+        self.rect.center = old_rect.center
         
 class SheetSprite(ImageSprite):
     def __init__(self,_sheet,_offset=0,_colorMap = {}):
