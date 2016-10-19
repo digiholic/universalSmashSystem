a6260c33fc6cb30bc860408ea0f858016f63cd89
Eugene Wang <eaglgenes101@gmail.com>
2016-10-19 15:42:02 -0700

    Still working on different panel kinds

--- menu/panel.py
+++ menu/panel.py
@@ -9,20 +9,17 @@ import difflib
 """
 This is the base UI element that everything else is based upon. It can also be initialized directly to obtain a boring ol' rectangle, with or without text. 
 """
-class panel(pygcurse.PygcurseTextbox):
-    def __init__(self, _surface, _parent=None, _region=(0,0,0,0), **kwargs):
+class panel(pygcurse.PygcurseSurface):
+    def __init__(self, _surface, _parent=None, _width=20, _height=1, corner=(0, 0), _font="unifont-9.0.02", _size=16):
         self.parent = _parent
+        self.corner = _corner
         if _parent is not None:
-            _region = (_region[0]+_parent.x, _region[1]+_parent.y, _region[2], _region[3])
             _parent.children.append(self)
+            self.corner = (_parent.corner[0]+self.corner[0], _parent.corner[1]+self.corner[1])
             bgcolor = _parent.bgcolor-pygame.Color(16, 16, 16)
         else:
-            bgcolor = _surface._bgcolor-pygame.Color(16, 16, 16)
-        if bgcolor.r+bgcolor.g+bgcolor.b <= 381:
-            fgcolor = pygame.Color(255, 255, 255)
-        else:
-            fgcolor = pygame.Color(0, 0, 0)
-        pygcurse.PygcurseTextbox.__init__(self, _surface, _region, border=None, fgcolor=fgcolor, bgcolor=bgcolor, **kwargs) #Who knows? Maybe we want captions, or margins, or what. 
+            bgcolor = pygame.Color(255, 255, 255)
+        pygcurse.PygcurseSurface.__init__(self, _width, _height, font=pygame.font.Font(settingsManager.createPath(_font+".ttf"),_size), fgcolor=pygame.Color(0, 0, 0), bgcolor=bgcolor, windowsurface=_surface) #Who knows? Maybe we want captions, or margins, or what. 
         self.children = []
         self.focused = None
 
@@ -33,7 +30,7 @@ class panel(pygcurse.PygcurseTextbox):
     """
     def update(self):
         # Put overriding update code here. 
-        pygcurse.PygcurseTextbox.update(self)
+        pygcurse.PygcurseSurface.update(self)
         if self.children:
             for child in self.children:
                 if child is not self.focused: child.update()
@@ -48,14 +45,19 @@ class panel(pygcurse.PygcurseTextbox):
         However, if it loses focus or we have no focusing child, see if it should focus on us. 
             If it does, then determine if any of out children should gain focus. Return true if we give a child focus. 
             Otherwise, pass false, and let our parent decide. 
-    If we have focus and none of our children returned True, run onMouseEvent. By default, this is nothing. 
-    Overriding this method is discouraged; if you want to customize event response, use the provided refocus, keepFocus, gainFocus, and onEvent methods. 
+    If we have focus and none of our children returned True, run onEvent. By default, this is nothing. 
+    Overriding this method is discouraged; if you want to customize event response, use the provided preFocus, refocus, keepFocus, gainFocus, and onEvent methods. 
     """
     def capture(self, _event):
         if self.focused is not None:
             if self.focused.capture(): return True
-            else: 
-                self.focused = None
+            else: self.focused = None
+        return preFocus(self, _event)
+
+    """
+    A starting function into the recursive function refocus. 
+    """
+    def preFocus(self, _event):
         if self.keepFocus(_event):
             if self.children:
                 for child in self.children:
@@ -69,9 +71,10 @@ class panel(pygcurse.PygcurseTextbox):
 
     """
     Recursive method to determine which child should gain focus if we need to switch focus. 
-    Can be overridden, but usally, overriding gainFocus should be enough. 
+    Can be overridden, but overriding gainFocus or preFocus should be enough. 
     """
     def refocus(self, _event):
+        self.former_focused = self.focused
         self.focused = None
         if self.gainFocus(_event):
             if self.children:
@@ -79,7 +82,7 @@ class panel(pygcurse.PygcurseTextbox):
                     if child.refocus(self, _event):
                         self.focused = child
                         return True
-            # If we got here, either we have no children, or none of our children gained focus. So let's run our mouse event! 
+            # If we got here, either we have no children, or none of our children gained focus. So let's run our event! 
             self.onEvent(_event)
             return True
         else: return False
@@ -88,34 +91,88 @@ class panel(pygcurse.PygcurseTextbox):
     Given the given event, returns if we should gain focus or not. 
     """
     def gainFocus(self, _event):
-        return _mouseEvent.type == pygame.MOUSEBUTTONUP and pygame.Rect(self.x, self.y, self.width, self.height).collidepoint(_mouseEvent.pos)
+        return _event.type == pygame.MOUSEBUTTONUP and self.getcoordinatesatpixel(_event.pos[0]-self.corner[0]. _event.pos[1]-self.corner[1]) != (None, None)
 
     """
     Given the given event, returns if we should keep focus or not. 
     """
     def keepFocus(self, _event):
-        return _mouseEvent.type != pygame.MOUSEBUTTONUP or pygame.Rect(self.x, self.y, self.width, self.height).collidepoint(_mouseEvent.pos)
+        return _event.type != pygame.MOUSEBUTTONUP or self.getcoordinatesatpixel(_event.pos[0]-self.corner[0]. _event.pos[1]-self.corner[1]) != (None, None)
 
     """
     While we are in focus, process the given event. 
-    The default behavior is to remove self-focus and pass the event to a parent. 
+    The default behavior is to remove self-focus. 
     """
     def onEvent(self, _event):
         if self.parent is not None:
             self.parent.focused = None
-            self.parent.onEvent(_event)
 
-"""A persistent text box object, which can be inputted to."""
+"""
+A text button that displays feedback if clicked.
+"""
+class button(panel):
+    def __init__(self, _surface, _parent=None, _width=20, _height=1, _corner=(0, 0), _font="unifont-9.0.02", _size=16, _optionDict=None)
+        self.panel.__init__(self, _surface, _parent, _width, _height, _corner, _font, _size)
+        self.putchars(_optionDict.keys[0], x=0, y=0)
+        self.option_dict= _optionDict
+        self.selection = 0
+        self.button_down = False
+
+    def update(self):
+        if self.button_down:
+            self.setfgcolor(self._bgcolor)
+            self.setbgcolor(self._fgcolor)
+            self.putchars(_optionDict.values[selection], x=0, y=0)
+        else:
+            self.setfgcolor(self._fgcolor)
+            self.setbgcolor(self._bgcolor)
+            self.putchars(_optionDict.keys[selection], x=0, y=0)
+        panel.update(self)
+
+    def setValue(self, _newSelect, _newDict=None):
+        if _newDict is not None:
+            self.option_dict = _newDict
+        self.selection = _newSelect
+
+    def getValue(self):
+        return self.option_dict.keys()[selection]
+
+    def gainFocus(self, _event):
+        if _event.type == pygame.MOUSEBUTTONDOWN and self.getcoordinatesatpixel(_event.pos[0]-self.corner[0]. _event.pos[1]-self.corner[1]) != (None, None):
+            return True
+        else:
+            return False
+
+    def keepFocus(self, _event):
+        if self.getcoordinatesatpixel(_event.pos[0]-self.corner[0]. _event.pos[1]-self.corner[1]) == (None, None) or not panel.keepFocus(self, _event):
+            self.button_down = False
+            return False
+        else:
+            return True
+
+    def onEvent(self, _event):
+        if _event.type = pygame.MOUSEBUTTONUP:
+            if self.button_down:
+                self.selection = (self.selection+1)%len(self.option_dict)
+            self.button_down = False
+        elif _event.type = pygame.MOUSEBUTTONDOWN:
+            self.button_down = True
+                
+
+"""
+A persistent text box object, which can be inputted to.
+"""
 class textEntry(panel):
-    def __init__(self, _surface, _parent, _region=(0, 0, 0, 0), _active=True, **kwargs):
-        self.panel.__init__(_surface, _parent, _region, **kwargs)
+    def __init__(self, _surface, _parent=None, _width=20, _height=1, _corner=(0, 0), _font="unifont-9.0.02", _size=16, _initText='', _active=False):
+        self.panel.__init__(self, _surface, _parent, _width, _height, _corner, _font, _size)
         self.input_obj = pygcuse.PygcurseInput(self)
-        self.input_obj.multiline = _region[3] > 1 #If it's a single line, don't scroll
+        self.input_obj.multiline = _height > 1 #If it's a single line, don't scroll
+        self.input_obj.buffer = list(_initText)
         self.active = _active
 
-    def display(self):
+    def update(self):
         self.input_obj.update()
-        self.update()
+        panel.update(self)
 
     def setValue(self, _value):
         self.input_obj.buffer = list(_value)
@@ -125,37 +182,65 @@ class textEntry(panel):
 
     def deactivate(self):
         self.active = False
-        #Modify colors
+        self._inputcursormode = None
 
-    def reactivate(self):
+    def activate(self):
         self.active = True
-        #Modify colors
-
-    def keyCapture(self, _event):
-        """Process a key event. Returns true if focus should be maintained, false if it should be lost"""
-        if not self.active:
+        self._inputcursormode = input_obj.insertMode and 'insert' or 'underline'
+        
+    def keepFocus(self, _event):
+        if not panel.keepFocus(self, _event):
             return False
-        elif _event.type == pygame.KEYDOWN and _event.key in [pygame.key.K_ESCAPE, pygame.key.K_UP, pygame.key.K_DOWN, pygame.key.K_HOME, pygame.key.K_END, pygame.key.K_PAGEUP, pygame.key.K_PAGEDOWN]:
+        elif _event.type in [pygame.KEYDOWN, pygame.KEYUP] and not input_obj.multiline and _event.key == pygame.key.K_RETURN:
             return False
-        elif _event.type == pygame.KEYUP and _event.key == pygame.key.K_RETURN:
+        elif _event.type in [pygame.KEYDOWN, pygame.KEYUP] and _event.key in [pygame.key.K_ESCAPE, pygame.key.K_UP, pygame.key.K_DOWN, pygame.key.K_HOME, pygame.key.K_END, pygame.key.K_PAGEUP, pygame.key.K_PAGEDOWN]:
+            return False
+        elif _event.type in [pygame.KEYDOWN, pygame.KEYUP]:
             return False
         else:
-            self.input_obj.sendkeyevent(_event)
             return True
 
-    def mouseCapture(self, _event):
-        """Process a mouse event. Returns true if focus should be maintained, false if it should be lost"""
-        if not self.active:
-            return False
-        elif _event.type == pygame.MOUSEBUTTONDOWN and self._windowsurface.get_rect(topleft=self.corner).collidepoint(_event.get_pos):
-            #TODO: Add code to adjust cursor position based on mouse position
+    def onEvent(self, _event):
+        if self.active and _event.type in [pygame.KEYDOWN, pygame.KEYUP]:
+            input_obj.sendkeyevent(event)
+
+"""
+A panel for holding interrelated panels, which can be switched between with the arrow keys. No scrolling capability. 
+"""
+class panelGrid(panel):
+    def __init__(self, _surface, _parent=None, _width=20, _height=10, corner=(0, 0), _font="unifont-9.0.02", _size=16):
+        self.panel.__init__(self, _surface, _parent, _width, _height, _corner, _font, _size)
+
+    def preFocus(self, _event):
+        if _event.type in [pygame.KEYUP, pygame.KEYDOWN]:
+            focus_index = self.children.index(self.focused)
+            if _event.type == pygame.KEYUP and _event.key == pygame.key.K_RETURN:
+                focus_index = (focus_index+1)%len(self.children)
+            elif _event.type == pygame.KEYDOWN and _event.key == pygame.key.K_UP:
+                if focus_index > 0: focus_index -= 1
+            elif _event.type == pygame.KEYDOWN and _event.key == pygame.key.K_DOWN:
+                if focus_index <= len(self.children): focus_index += 1
+            elif _event.type == pygame.KEYDOWN and _event.key == pygame.key.K_PAGEUP:
+                focus_index = min(0, focus_index-5)
+            elif _event.type == pygame.KEYDOWN and _event.key == pygame.key.K_PAGEDOWN:
+                focus_index = max(focus_index+5, len(self.children)-1)
+            elif _event.type == pygame.KEYDOWN and _event.key == pygame.key.K_HOME:
+                focus_index = 0
+            elif _event.type == pygame.KEYDOWN and _event.key == pygame.key.K_END:
+                focus_index = len(self.children)-1
+            elif _event.type == pygame.KEYDOWN and _event.key == pygame.key.K_ESCAPE:
+                self.focused = None
+                return False
+            self.focused = self.children[focus_index]
             return True
-        elif _event.type == pygame.MOUSEBUTTONDOWN
-            return False
-        else
-            return True
+        else return panel.preFocus(self, _event)
             
-"""A temporary drop-down menu. Appears when created, should disappear when a choice is made"""
+"""
+A panel for getting a view of a child panel. Tries its best to keep the focused panel in view. 
+"""
+
+"""
+#A temporary drop-down menu. Appears when created, should disappear when a choice is made
 class searchOption(pygcurse.PygcurseSurface):
     def __init__(self, _surface, _font="unifont-9.0.02", _size=16, _length=20, _height=29, _corner=(0, 0), _options=None):
         self.screen = _surface
@@ -169,7 +254,7 @@ class searchOption(pygcurse.PygcurseSurface):
         else
             self.options = list()
 
-    def display(self):
+    def update(self):
         # The design of the display is that the current selection is centered whenever possible. 
         # The selection view ranges from 0 to self.selection_height-1, and we want to position
         # the selection at self.selection_height//2. 
@@ -215,10 +300,6 @@ class searchOption(pygcurse.PygcurseSurface):
         self.update()
         self.blitto(self.screen, self.corner)
 
-    """
-    Returns the closest match in options to the given key. 
-    Probably should be overridden if needed. 
-    """
     def autoSelect(self, _key=""):
         wordlist = difflib.get_close_matches(_key, self.options, n=1, cuttoff=0)
         if len(wordlist) == 0:
@@ -238,3 +319,4 @@ class searchOption(pygcurse.PygcurseSurface):
 
     #cdef keyCapture(self, _event):
     
+"""
