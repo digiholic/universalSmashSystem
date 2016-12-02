690371548b504ad0f6b59b41eea6ad7496188b3d
Eugene Wang <eaglgenes101@gmail.com>
2016-12-01 16:52:13 -0800

    Starting on it

--- engine/controller.py
+++ engine/controller.py
@@ -1,65 +1,135 @@
 import settingsManager
 import pygame
+import re
 
 class BaseController():
-    def __init__(self,_bindings):
-        self.keys_to_pass = []
-        self.keys_to_release = []
-        self.keys_held = []
+    init_dict = {
+                 'MoveHor': '(-1, \'MoveHor\', 0)',
+                 'MoveVert': '(-1, \'MoveVert\', 0)',
+                 'ActHor': '(-1, \'ActHor\', 0)',
+                 'ActVert': '(-1, \'ActVert\', 0)',
+                 'attack': '(-1, \'attack\', 0)',
+                 'special': '(-1, \'special\', 0)',
+                 'jump': '(-1, \'jump\', 0)',
+                 'shield': '(-1, \'shield\', 0)',
+                 'taunt': '(-1, \'taunt\', 0)'
+                }
+    current_dict = {
+                    'MoveHor': 0,
+                    'MoveVert': 0,
+                    'ActHor': 0,
+                    'ActVert': 0,
+                    'attack': 0,
+                    'special': 0,
+                    'jump': 0,
+                    'shield': 0,
+                    'taunt': 0
+                   }
+    def __init__(self,_bindings,_windows):
         self.key_bindings = _bindings
-        self.type = 'Keyboard'
-        self.target = None
-        
-        self.buffer = [[]]
-        self.last_index = 0
-      
-      
-    def linkObject(self,_object):
-        self.target = _object
+        self.windows = _windows
+        self.type = 'Base'
+        self.current_state = self.current_dict
+        self.initials = self.init_dict
+        self.buffer = list()
+        self.action_num = 0
+        self.frame_num = 0
     
     def flushInputs(self):
-        self.keys_to_pass = []
-        self.keys_to_release = []
-        self.keys_held = []
-        
-        self.buffer = [[]]
-        self.last_index = 0
-        
-    def getInputs(self,_event,_push=True, _outputOnRelease=True):
-        if _event.type not in [pygame.KEYDOWN, pygame.KEYUP]:
-            return None
-        output = True
-        k = self.key_bindings.get(_event.key)
-        if k:
-            if _event.type == pygame.KEYDOWN:
-                if _push: self.keys_to_pass.append(k)
-                if k not in self.keys_held: self.keys_held.append(k)
-            elif _event.type == pygame.KEYUP:
-                output = _outputOnRelease and output
-                if _push: self.keys_to_release.append(k)
-                if k in self.keys_held: self.keys_held.remove(k)
-        if output: return k
-        return None
+        self.current_state = self.current_dict
+        self.initials = self.init_dict
+        self.buffer = list()
+        self.action_num = 0
+        self.frame_num = 0
+
+    # Please call every time a primitive input state gets modified
+    def pushPrimitive(self,_primitive,_state):
+        last_state = self.current_state[_primitive]
+        if _primitive in ('MoveHor', 'MoveVert', 'ActHor', 'ActVert'):
+            if _state < -self.windows['tap_threshold']: state_num = -4
+            elif _state < -self.windows['press_threshold']: state_num = -3
+            elif _state < -self.windows['tilt_threshold']: state_num = -2
+            elif _state < -self.windows['nudge_threshold']: state_num = -1
+            elif _state <= self.windows['nudge_threshold']: state_num = 0
+            elif _state <= self.windows['tilt_threshold']: state_num = 1
+            elif _state <= self.windows['press_threshold']: state_num = 2
+            elif _state <= self.windows['tap_threshold']: state_num = 3
+            else: state_num = 4
+            
+            if last_state < -self.windows['tap_threshold']: last_num = -4
+            elif last_state < -self.windows['press_threshold']: last_num = -3
+            elif last_state < -self.windows['tilt_threshold']: last_num = -2
+            elif last_state < -self.windows['nudge_threshold']: last_num = -1
+            elif last_state <= self.windows['nudge_threshold']: last_num = 0
+            elif last_state <= self.windows['tilt_threshold']: last_num = 1
+            elif last_state <= self.windows['press_threshold']: last_num = 2
+            elif last_state <= self.windows['tap_threshold']: last_num = 3
+            else: last_num = 4
+        elif _primitive in ('attack', 'special', 'jump', 'shield', 'taunt'):
+            if _state > 0: state_num = 1
+            else: state_num = 0
+
+            if last_state > 0: last_num = 1
+            else: last_num = 0
+
+        if state_num != last_num:
+            self.buffer.append(0, (action_num, str((self.action_num, _primitive, state_num))))
+        self.current_state[_primitive] = _state
+        self.action_num += 1
+
+    def pumpInput(self):
+        self.buffer.append(0, (action_num, str((self.action_num, 'frame', self.frame_num))))
+        self.frame_num += 1
+        self.action_num += 1
+
+    def pushInput(self,_event,_outputOnRelease=True):
+        raise NotImplementedError
     
-    def get(self,_key):
+    def getKeyAction(self,_key):
         return self.key_bindings.get(_key)
-    
-    def passInputs(self):
-        if self.target:
-            for key in self.keys_to_pass:
-                self.target.keyPressed(key)
-            for key in self.keys_to_release:
-                self.target.keyReleased(key)
-        self.keys_to_pass = []
-        self.keys_to_release = []
-        
-    def getKeysForAction(self,_action):
+
+    def getActionKeys(self,_action):
         list_of_bindings = []
         for binding,name in self.key_bindings.items():
             if name == _action:
                 list_of_bindings.append(settingsManager.getSetting().key_id_map[binding])
         return list_of_bindings
-    
+
+    def getWindow(self,_key):
+        return self.windows.get(_key)
+
+    def getState(self,_key):
+        return self.current_state.get(_key)
+        
+    def parseBuffer(self,_compiledRegexList):
+        buffer_list = list()
+        for primitive_change in reversed(self.buffer):
+            buffer_list.append(primitive_change[1])
+        for primitive in self.initials:
+            buffer_list.append(self.initials[primitive])
+        search_str = ''.join(buffer_list)
+        matches = {rex: rex.search(search_str) for rex in _compiledRegexList}
+        matches = filter(None, matches)
+        if len(matches) == 0:
+            return -1 # No match
+        earliest_regex = min(matches, key=lambda k: matches[k].start())
+        # Find the most recent action referenced by the match, and remove earlier actions
+        check_string = matches[earliest_regex].group()
+        
+        act_string = check_string[1:check_string.find(',')-1]
+        try:
+            act_num = int(act_string)
+            for index in range(len(self.buffer)):
+                reverse_index = len(self.buffer)-1-index
+                if act_num > self.buffer[reverse_index][0]:
+                    init_dict[self.buffer[reverse_index][1][1]] = self.buffer[reverse_index][1]
+                    del self.buffer[reverse_index]
+                else:
+                    break # Going on won't do us any good
+        except ValueError:
+            print("Buffer not pruned, as an action number could not be determined. This most likely means the regex match didn't align to an input tuple. ")
+            pass
+        return _compiledRegexList.index(earliest_regex)
     
 class Controller(BaseController):
     def __init__(self,_bindings,_timing_window = dict()):
